c51439a0b4915b32d2508807aac128eb61f47ab5
loading <93635517+blueberrysmoooothie@users.noreply.github.com>
2023-07-28 21:11:28 +0900

    test

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return render_template('./index.html')
+    return render_template("./index.html")
+
+
+@app.route("/get_station_info/<station_keyword>", methods=["GET"])
+def get_station_info(station_keyword):
+    return station_keyword
+
+
+if __name__ == "__main__":
+    app.run()
